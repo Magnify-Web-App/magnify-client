ae1510c723d16b43791144f3bd318b5eba671705
Kardkasem Saenyen <mos311063@hotmail.com>
2020-01-22 18:22:07 +1100

    Updates

---
+++ precache-manifest.1d84a6b4306f5e222d8e6015443b4fa1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ab7ecabb8c593dd482b2688735d0f4",
+    "url": "/magnify-client/index.html"
+  },
+  {
+    "revision": "a62bed4ac38f20d41a00",
+    "url": "/magnify-client/static/css/main.9503f1b5.chunk.css"
+  },
+  {
+    "revision": "f945008e53ac681e2c22",
+    "url": "/magnify-client/static/js/2.d6f68a24.chunk.js"
+  },
+  {
+    "revision": "31ed9f602e2907285e44f74bdb946da9",
+    "url": "/magnify-client/static/js/2.d6f68a24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a62bed4ac38f20d41a00",
+    "url": "/magnify-client/static/js/main.3efd0cd5.chunk.js"
+  },
+  {
+    "revision": "6061961a713b7635c017",
+    "url": "/magnify-client/static/js/runtime-main.17859e17.js"
+  },
+  {
+    "revision": "3f125ede620cca26ad9ea76497db1ef7",
+    "url": "/magnify-client/static/media/2895265.3f125ede.jpg"
+  },
+  {
+    "revision": "649bc464756dd35d9a6d6aede9c4b9d2",
+    "url": "/magnify-client/static/media/312.649bc464.jpg"
+  },
+  {
+    "revision": "aa259fc9df12ac4392be9161e7423200",
+    "url": "/magnify-client/static/media/images.aa259fc9.jpeg"
+  },
+  {
+    "revision": "4060779129691a1e66da05c582f42749",
+    "url": "/magnify-client/static/media/public.40607791.jpg"
+  },
+  {
+    "revision": "8d991bd2955f520eee33741ae95af29e",
+    "url": "/magnify-client/static/media/survey.8d991bd2.jpg"
+  },
+  {
+    "revision": "afdcf291d7781d0689743a28bbca1b28",
+    "url": "/magnify-client/static/media/survey.afdcf291.jpeg"
+  }
+]);
